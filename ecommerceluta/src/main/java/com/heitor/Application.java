46ee0465d4687ea93ab787255548adf8a4fb45fc
hsfm <hsfm@EU3146013017369.local>
2025-11-04 20:23:52 -0300

    Etapa 1: Configura conexão com PostgreSQL e ajusta application.properties

---
+++ ecommerceluta/src/main/java/com/heitor/Application.java
@@ -0,0 +1,10 @@
+package com.heitor;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
